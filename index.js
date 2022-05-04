07ef34d2851f7ae664d19772ee530b0889a90190
Elijah Bate <elijahbate@gmail.com>
2022-05-04 10:00:20 +0000

    Test somthing

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { handleEvent } from "flareact";
 
+window = window || {};
+if (!window.requestAnimationFrame) {
+  window.requestAnimationFrame = function () {
+    console.log("filler func");
+  };
+}
 /**
  * The DEBUG flag will do two things that help during development:
  * 1. we will skip caching on the edge, which makes it easier to
@@ -12,7 +18,11 @@ const DEBUG = false;
 addEventListener("fetch", (event) => {
   try {
     event.respondWith(
-      handleEvent(event, require.context("./pages/", true, /\.(js|jsx|ts|tsx)$/), DEBUG)
+      handleEvent(
+        event,
+        require.context("./pages/", true, /\.(js|jsx|ts|tsx)$/),
+        DEBUG
+      )
     );
   } catch (e) {
     if (DEBUG) {
