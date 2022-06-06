7b8883f67a962b52974ba88133fb72fc8d8c57a4
Elijah Bate <elijahbate@gmail.com>
2022-06-06 21:51:20 +0000

    More polyfil

--- index.js
+++ index.js
@@ -10,24 +10,86 @@ import { handleEvent } from "flareact";
 const DEBUG = false;
 
 addEventListener("fetch", (event) => {
-  global.window = global.window || {};
-  window = window || {};
-  window.requestAnimationFrame = () => {
-    console.log("Polyfil executed");
-  };
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function () {
-      console.log("filler func");
-    };
-  }
+  // global.window = global.window || {};
+  // window = window || {};
+  // window.requestAnimationFrame = () => {
+  //   console.log("Polyfil executed");
+  // };
+  // if (!window.requestAnimationFrame) {
+  //   window.requestAnimationFrame = function () {
+  //     console.log("filler func");
+  //   };
+  // }
+  (function () {
+    var lastTiFme = 0;
+    var vendors = ["ms", "moz", "webkit", "o"];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame =
+        window[vendors[x] + "RequestAnimationFrame"];
+      window.cancelAnimationFrame =
+        window[vendors[x] + "CancelAnimationFrame"] ||
+        window[vendors[x] + "CancelRequestAnimationFrame"];
+    }
+
+    if (!window.requestAnimationFrame)
+      window.requestAnimationFrame = function (callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function () {
+          callback(currTime + timeToCall);
+        }, timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = function (id) {
+        clearTimeout(id);
+      };
+  })();
+
   try {
-    event.respondWith(
-      handleEvent(
+    event.respondWith(() => {
+      global.window = {};
+      window = window || {};
+      (function () {
+        var lastTiFme = 0;
+        var vendors = ["ms", "moz", "webkit", "o"];
+        for (
+          var x = 0;
+          x < vendors.length && !window.requestAnimationFrame;
+          ++x
+        ) {
+          window.requestAnimationFrame =
+            window[vendors[x] + "RequestAnimationFrame"];
+          window.cancelAnimationFrame =
+            window[vendors[x] + "CancelAnimationFrame"] ||
+            window[vendors[x] + "CancelRequestAnimationFrame"];
+        }
+
+        if (!window.requestAnimationFrame)
+          window.requestAnimationFrame = function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () {
+              callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+          };
+
+        if (!window.cancelAnimationFrame)
+          window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+          };
+      })();
+
+      return handleEvent(
         event,
         require.context("./pages/", true, /\.(js|jsx|ts|tsx)$/),
         DEBUG
-      )
-    );
+      );
+    });
   } catch (e) {
     if (DEBUG) {
       return event.respondWith(
